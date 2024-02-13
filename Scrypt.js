fb3fdf5718830b4644231b8ef41f12ada28d4580
“CaesarBourne” <caesaradek@gmail.com>
2024-02-13 13:36:35 +0100

    scrypt and CSPRING CRYPTO Incorporation.JS KEY GENERATION

--- Scrypt.js
+++ Scrypt.js
@@ -1,4 +1,21 @@
 const { scrypt } = require("ethereum-cryptography/scrypt");
-const { utf8ToBytes } = require("ethereum-cryptography/utils");
+const { utf8ToBytes, bytesToHex } = require("ethereum-cryptography/utils");
 
-const { bytesToHex } = require("ethereum-cryptography/utils");
+// const { bytesToHex } = require("ethereum-cryptography/utils");
+
+async function getScrypt() {
+  console.log(
+    bytesToHex(
+      await scrypt(
+        utf8ToBytes("Calyptus"),
+        utf8ToBytes("Hello"),
+        1024,
+        8,
+        1,
+        32
+      )
+    )
+  );
+}
+
+getScrypt();
