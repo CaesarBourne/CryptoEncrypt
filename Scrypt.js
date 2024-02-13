c6f65d7b4694f7a16317a0e8eeb7a558f5d26df0
“CaesarBourne” <caesaradek@gmail.com>
2024-02-13 11:33:49 +0100

    scRYPT.JS KEY GENERATION

---
+++ Scrypt.js
@@ -0,0 +1,4 @@
+const { scrypt } = require("ethereum-cryptography/scrypt");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+const { bytesToHex } = require("ethereum-cryptography/utils");
